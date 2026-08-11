2e793c3418e675c6d0ca87f57498c64c7d1b7bcb
Lokesh08112003 <arieslokesh8@gmail.com>
2026-08-11 17:15:59 +0530

    Test automatic CI trigger

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,7 +3,7 @@ package com.example;
 public class App {
 
     public static String getMessage() {
-        return "CI/CD Pipeline is working!";
+        return "CI/CD Pipeline is working successfully!";;
     }
 
     public static void main(String[] args) {
